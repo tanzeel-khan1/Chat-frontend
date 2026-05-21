e4112bb9d07fd8d04120cb4e2f7efc27144539a5
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-05-21 11:52:05 +0500

    user f

--- src/home/left/User.jsx
+++ src/home/left/User.jsx
@@ -1,3 +1,28 @@
+// import React from "react";
+// import Users from "../Users";
+// import GetAllUsers from "../../context/GetAllUsers";
+
+// const User = ({ setShowSidebar }) => {
+//   const [allUser, loading] = GetAllUsers();
+
+//   console.log(allUser);
+//   return (
+//     <div
+//       style={{ maxHeight: "calc(100vh - 200px)" }}
+//       className="flex-1 overflow-y-auto"
+//     >
+      
+//       {allUser.map((user, index) => {
+//   return (
+//     <Users key={index} user={user} setShowSidebar={setShowSidebar} />
+//   );
+// })}
+//     </div>
+//   );
+// };
+
+// export default User;
+
 import React from "react";
 import Users from "../Users";
 import GetAllUsers from "../../context/GetAllUsers";
@@ -6,20 +31,24 @@ const User = ({ setShowSidebar }) => {
   const [allUser, loading] = GetAllUsers();
 
   console.log(allUser);
+
   return (
     <div
       style={{ maxHeight: "calc(100vh - 200px)" }}
       className="flex-1 overflow-y-auto"
     >
-      {/* {allUser.map((user, index) => {
-        return 
-        <Users key={index} user={user} setShowSidebar={setShowSidebar} />;
-      })} */}
-      {allUser.map((user, index) => {
-  return (
-    <Users key={index} user={user} setShowSidebar={setShowSidebar} />
-  );
-})}
+      {loading ? (
+        <p className="text-center">Loading...</p>
+      ) : (
+        Array.isArray(allUser) &&
+        allUser.map((user, index) => (
+          <Users
+            key={index}
+            user={user}
+            setShowSidebar={setShowSidebar}
+          />
+        ))
+      )}
     </div>
   );
 };
